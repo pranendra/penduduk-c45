21185566a90cd0f2e39c631c80103cabb1eac57e
pranendra <samuel.benner@my.gavilan.edu>
2020-07-13 21:55:17 +0700

    Klasifikasi Sensus Penduduk dengan Prediksi C45

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Configuration Database
+ */
+return array(
+    'host' => "localhost",
+    'username' => "root",
+    'password' => "",
+    'dbname' => "penduduk_c45",
+    // 'port' => "", // sementara menggunakan port bawaan
+);
+
+
